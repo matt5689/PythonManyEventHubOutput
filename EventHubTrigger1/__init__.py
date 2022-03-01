03a37502c1d8302a5eb8d66738bade4c932517c5
Matthew Cavallo <macavall@microsoft.com>
2022-02-28 21:51:11 -0600

    Using List for string to return List of EventHubMessages

--- EventHubTrigger1/__init__.py
+++ EventHubTrigger1/__init__.py
@@ -5,10 +5,13 @@ import azure.functions as func
 
 
 def main(events: List[func.EventHubEvent]):
+    eventsList= []
     for event in events:
         logging.info('Python EventHub trigger processed an event: %s',
                         event.get_body().decode('utf-8'))
-        return event.get_body().decode('utf-8')
+        eventsList.append(event.get_body().decode('utf-8'))
+    
+    return eventsList
 
 # def main(events: List[func.EventHubEvent]) -> str:
 #     return [events[0].get_body().decode('utf-8'), events[1].get_body().decode('utf-8')]
